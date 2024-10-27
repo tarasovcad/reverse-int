1f3508efbd941260eb4f347e3a55c80b9a07f2e1
tarasovcad <tarasovcad@gmail.com>
2024-10-27 14:19:40 -0500

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    const isNegative = n < 0;
+    const reversedStr = Math.abs(n).toString().split('').reverse().join('');
+    const reversedNum = parseInt(reversedStr, 10);
+    return isNegative ? reversedNum : reversedNum;
 }
